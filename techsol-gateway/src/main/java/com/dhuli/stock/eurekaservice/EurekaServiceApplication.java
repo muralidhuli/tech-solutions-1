b555e5202dd69db880a9ed34eb681a836a85d0df
muralidhuli <muralidhuli@users.noreply.github.com>
2020-06-19 15:16:30 -0700

    Adding gateway for techsolutions added zuul gateway and eureka discovery registry Added comment to test stash

---
+++ techsol-gateway/src/main/java/com/dhuli/stock/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,18 @@
+package com.dhuli.stock.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+//main boot class for gateway 
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
+
