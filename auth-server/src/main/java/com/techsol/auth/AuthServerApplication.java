d8271c28b09fb5a6bee75dea27d0ec6e1d40fde9
Murali Dhuli <murali.dhuli@gmail.com>
2020-07-20 11:12:05 -0700

    Added comment in oauth serverclass

--- auth-server/src/main/java/com/techsol/auth/AuthServerApplication.java
+++ auth-server/src/main/java/com/techsol/auth/AuthServerApplication.java
@@ -2,7 +2,7 @@ package com.techsol.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Main class of Auth server
 @SpringBootApplication
 public class AuthServerApplication {
 
